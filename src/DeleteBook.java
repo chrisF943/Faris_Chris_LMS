f612dd1ab3d7dff33255d3b89b8228ba82fc6fc9
chris <chris.faris@icloud.com>
2023-11-12 12:01:15 -0500

    updated

--- src/DeleteBook.java
+++ src/DeleteBook.java
@@ -1,107 +1,104 @@
 import javax.swing.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /*
 Chris Faris
 Software Development 1
-10/20/2023
+11/10/2023
 class name: DeleteBook
-contains the methods to delete a book from the collection, called in the Main class when
-the user wants to delete a book
+contains the method prompting the user to enter a barcode or title of a book they want to delete
+as well as the methods for deleting a book from the database for both of those scenarios
+and the method for executing the SQL statement
  */
 public class DeleteBook {
 
-    /*
-     method name: selectBook
-     asks the user to enter an ID or title of a book to delete, iterates through the collection and
-     if it is found deletes that book from the collection and updates the collection
-     passed in the created ArrayList as a String parameter, returns the updated collection
+    /*asks the user to enter a barcode or title of a book to delete, iterates through the database and
+     if it is found the appropriate method is called and SQL statement is executed
+     has no parameters and no return
      */
-    public static ArrayList<String> selectBook(ArrayList<String> collection) {
-        Scanner input = new Scanner(System.in);
+    public static void selectBook() {
         String userInput;
-        boolean deleted = false;
-        ArrayList<String> updatedCollection = new ArrayList<>();
+        String url = "jdbc:sqlite:/Users/chrisfaris/collection.db";
+        boolean deleted;
         //checks if user input is a integer and calls the deleteById method, if not calls the deleteByTitle method
         do {
             userInput = JOptionPane.showInputDialog("Please enter the ID number or title of the book you want to delete: ");
 
             if (userInput.matches("\\d+")) {
-                int idToDelete = Integer.parseInt(userInput);
-                deleted = deleteById(collection, updatedCollection, idToDelete);
+                int barcodeToDelete = Integer.parseInt(userInput);
+                deleted = deleteByBarcode(barcodeToDelete, url);
             }else {
-                deleted = deleteByTitle(collection, updatedCollection, userInput);
+                deleted = deleteByTitle(userInput, url);
             }
             if (!deleted) {
-                JOptionPane.showMessageDialog(null, "That book does not exist in the collection, please try again");
+                JOptionPane.showMessageDialog(null, "That book does not exist in the database, please try again");
             }
         }while (!deleted);
-        JOptionPane.showMessageDialog(null, "Book has successfully been checked out from the collection. \n");
-        return updatedCollection;
+        JOptionPane.showMessageDialog(null, "Book has successfully been deleted from the database. \n");
     }//end selectBook
 
-    /*
-     method name: deleteById
-     Deletes a book from the collection based on its ID.
-     Returns true if the book was found and deleted; otherwise, returns false.
-     Parameters are both the collection and the updated collection ArrayLists and the id
+    /*is called if an instance of an integer is detected, contains the SQL statement to delete a book by barcode
+    the barcode of the book is passed in as well as the url of the database, returns the executeDeleteSQL method
+    with the parameters of the barcode and the SQL statement
      */
+    public static boolean deleteByBarcode(int barcode, String url) {
+        String sql = "DELETE FROM books WHERE barcode = ?";
+        return executeDeleteSQL(barcode, sql);
+    }//end deleteByBarcode
 
-    public static boolean deleteById(ArrayList<String> collection, ArrayList<String> updatedCollection, int id) {
-        boolean found = false;
-        /*searches through the collection for ID number entered by looking at first value before first comma
-          if the book exists then found is set to true and the book is deleted, else the collection is kept unchanged
-        */
-        for (String book: collection) {
-            String[] bookDetails = book.split(",");
-            if (bookDetails.length >= 1) {
-                int bookId;
-                try {
-                    bookId = Integer.parseInt(bookDetails[0]);
-                }catch (NumberFormatException e) {
-                    updatedCollection.add(book);
-                    continue;
-                }//end try catch
-                if (bookId == id) {
-                    found = true;
-                }//end inner if
-                else {
-                    updatedCollection.add(book);
-                }//end else
-            }//end outer if
-        }//end for
-        return found;
-    }//end deleteById
-
-    /*
-     method name: deleteByTitle
-     Deletes a book from the collection based on its title.
-     Returns true if the book was found and deleted; otherwise, returns false.
-     Parameters are both the collection and the updated collection ArrayLists and the title
-     */
-
-    public static boolean deleteByTitle(ArrayList<String> collection, ArrayList<String> updatedCollection, String title) {
-        boolean found = false;
-        /*searches through the collection for title entered by looking at value after first comma
-          if the book exists then found is set to true and the book is deleted, else the collection is kept unchanged
-          equalsIgnoreCase is used to avoid case sensitivity between user input and book title in the collection
-        */
-        for (String book: collection) {
-            String[] bookDetails = book.split(",");
-            if (bookDetails.length >= 2) {
-                String bookTitle = bookDetails[1].trim();
-                if (bookTitle.equalsIgnoreCase(title)) {
-                    found = true;
-                }//end inner if
-                else {
-                    updatedCollection.add(book);
-                }//end else
-            }//end outer if
-        }//end for
-        return found;
+    /*is called if an instance of a string is detected, contains the SQL statement to delete a book by title
+    the title of the book is passed in as well as the url of the database, returns the executeDeleteSQL method
+    with the parameters of the title and the SQL statement
+    */
+    public static boolean deleteByTitle(String title, String url) {
+        String sql = "DELETE FROM books WHERE title = ?";
+        return executeDeleteSQL(title, sql);
     }//end deleteByTitle
 
+
+    /*connects to the database and executes the appropriate SQL statement for deleting a book, based off
+    the value parameter passed in which will either be a barcode or title
+     */
+    public static boolean executeDeleteSQL(Object value, String sql) {
+        String url = "jdbc:sqlite:/Users/chrisfaris/collection.db";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = DriverManager.getConnection(url);
+            preparedStatement = connection.prepareStatement(sql);
+
+            //if the value is an integer, set the value as an integer, else set the value as a string
+            if (value instanceof Integer) {
+                preparedStatement.setInt(1, (Integer) value);
+            } else if (value instanceof String) {
+                preparedStatement.setString(1, (String) value);
+            }
+
+            int rowsDeleted = preparedStatement.executeUpdate();
+            return rowsDeleted > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }//end try catch
+        //close resources
+        finally {
+                try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }//end try catch
+        }//end finally
+    }//end executeDeleteSQL
 
 }//end class
 
--- src/Main.java
+++ src/Main.java
@@ -3,34 +3,23 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-
-import java.util.Scanner;
+import java.sql.*;
 
 /*
 Chris Faris
 Software Development 1
-10/20/2023
+11/10/2023
 class name: Main
-this class contains the main method that runs the LMS GUI, the program loads data from a text file
-into an ArrayList, and users can add, delete, checkout, and checkin books and view the collection
-all changes are written to the text file
+this class contains the main method that runs the LMS GUI, as well as the methods for adding, checking in
+and checking out books, and the method for communicating with the database when a new book is added
 */
 
 public class Main {
     private JFrame frame;
     private JTextArea outputTextArea;
-    private ArrayList<String> collection;
     private JPanel mainPanel;
 
     public Main() {
-        collection = ReadFile.getBooks("BookCollection.txt");
-
-       /* try{
-            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-        }catch(Exception e){
-            e.printStackTrace();
-        } */
 
         //sets up the frame and panel properties, adds a header message and a message prompt to select an option
 
@@ -77,7 +66,7 @@ public class Main {
         JButton viewButton = new JButton("View Collection");
         viewButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                displayCollection(collection);
+                displayCollection();
             }//end actionPerformed
         });
         viewButton.setFont(buttonFont);
@@ -85,8 +74,7 @@ public class Main {
         JButton addButton = new JButton("Add Book");
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                addBook(collection);
-                ReadFile.writeFile(collection, "BookCollection.txt");
+                addBook();
             }//end actionPerformed
         });
         addButton.setFont(buttonFont);
@@ -94,8 +82,7 @@ public class Main {
         JButton deleteButton = new JButton("Delete Book");
         deleteButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                collection = DeleteBook.selectBook(collection);
-                ReadFile.writeFile(collection, "BookCollection.txt");
+                DeleteBook.selectBook();
             }//end actionPerformed
         });
         deleteButton.setFont(buttonFont);
@@ -103,8 +90,7 @@ public class Main {
         JButton checkoutButton = new JButton("Checkout Book");
         checkoutButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                collection = DeleteBook.selectBook(collection);
-                ReadFile.writeFile(collection, "BookCollection.txt");
+                checkOutBook();
             }//end actionPerformed
         });
         checkoutButton.setFont(buttonFont);
@@ -112,8 +98,7 @@ public class Main {
         JButton checkinButton = new JButton("Checkin Book");
         checkinButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                addBook(collection);
-                ReadFile.writeFile(collection, "BookCollection.txt");
+                checkInBook();
             }//end actionPerformed
         });
         checkinButton.setFont(buttonFont);
@@ -128,52 +113,267 @@ public class Main {
     }//end Main
 
 
-    /*altered for use with GUI, prints out current contents of the collection
-    ArrayList type String collection is parameter with no return
+    /*altered for use with GUI and database, connects to the database and prints all contents via a
+    SELECT statement with no parameters and returns no return
     */
-    public void displayCollection(ArrayList<String> collection) {
-        outputTextArea.setText("Here is the current book collection:\n");
-        outputTextArea.append("----------------------------------------------\n");
-        for (String book : collection) {
-            outputTextArea.append(book + "\n");
-            outputTextArea.append("----------------------------------------------\n");
-        }//end for
+    public void displayCollection() {
+        String url = "jdbc:sqlite:/Users/chrisfaris/collection.db";
+
+        outputTextArea.setText("");
+
+        try (Connection connection = DriverManager.getConnection(url)) {
+            // Create a Statement object for executing SQL queries
+            Statement statement = connection.createStatement();
+
+            // SQL query to select all records from the "books" table
+            String query = "SELECT * FROM books";
+
+            // Execute the query and get the result set
+            ResultSet resultSet = statement.executeQuery(query);
+
+            outputTextArea.append("Here is the current collection:\n");
+            outputTextArea.append("-------------------------------------------------------------------------------------\n");
+
+            // Loop through the result set and print the contents
+            while (resultSet.next()) {
+                int barcode = resultSet.getInt("barcode");
+                String title = resultSet.getString("title");
+                String author = resultSet.getString("author");
+                String genre = resultSet.getString("genre");
+                String status = resultSet.getString("status");
+                String dueDate = resultSet.getString("due_date");
+
+                outputTextArea.append("Barcode: " + barcode + " ");
+                outputTextArea.append("Title: " + title + " ");
+                outputTextArea.append("By: " + author + " ");
+                outputTextArea.append("Genre: " + genre + " ");
+                outputTextArea.append("Status: " + status + " ");
+                outputTextArea.append("Due Date: " + dueDate + " ");
+                outputTextArea.append("\n");
+                outputTextArea.append("-------------------------------------------------------------------------------------\n");
+            }//end while
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }//end try catch
     }//end displayCollection
 
-    /*also altered for use with GUI, asks user for book details, creates it and adds it to the collection
-    if details are valid, also has ArrayList type String collection as a parameter with no return
+    /*also altered for use with GUI and database, connects to the database and adds a book to it
+    with details provided by the user
     */
-    public void addBook(ArrayList<String> collection) {
-        int id;
+    public void addBook() {
+        String url = "jdbc:sqlite:/Users/chrisfaris/collection.db";
+
+        int barcode = 0;
         String title;
         String author;
-        id = Integer.valueOf(JOptionPane.showInputDialog("Please enter an ID number:"));
-        if (doesIdExist(collection, id)) {
-            JOptionPane.showMessageDialog(null, "Cannot use an ID number that already exists.");
-            id = Integer.valueOf(JOptionPane.showInputDialog("Please enter an ID number:"));
+        String genre;
+        String status = "checked in";
+        String dueDate = "NULL";
+
+        String barcodeInput = JOptionPane.showInputDialog("Please enter a barcode:");
+        if (barcodeInput == null) {
+            // User clicked Cancel or closed the input dialog
+            return;
         }//end if
+
+        // Try to parse the barcode input
+        try {
+            barcode = Integer.parseInt(barcodeInput);
+            if (doesBarcodeExist(barcode, url)) {
+                JOptionPane.showMessageDialog(null, "A book with this barcode already exists.");
+                return;
+            }
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid barcode. Please enter a valid number.");
+            return;
+        }//end try catch
+
         title = JOptionPane.showInputDialog("Please enter the title of the book:");
         author = JOptionPane.showInputDialog("Please enter the author of the book:");
-        String book = id + "," + title + "," + author;
-        collection.add(book);
-        ReadFile.writeFile(collection, "BookCollection.txt");
-        JOptionPane.showMessageDialog(null, "Book has successfully been checked in to the collection.");
+        genre = JOptionPane.showInputDialog("Please enter the genre of the book:");
+
+        // Create a new book entry as a String
+        String book = barcode + "," + title + "," + author + "," + genre + "," + status + "," + dueDate;
+
+        // Add the book to the database
+        if (insertBookToDatabase(book)) {
+            JOptionPane.showMessageDialog(null, "Book has successfully been added to the collection.");
+        } else {
+            JOptionPane.showMessageDialog(null, "Failed to add the book to the collection.");
+        }//end if else
     }//end addBook
 
 
-    /*iterates through the collection to see if the id entered by user already exists, returns true if it does
-    else returns false, also has ArrayList type String collection as a parameter and the id as an int
+    /*iterates through the database to see if the id entered by user already exists, returns true if it does
+    else returns false, also has the barcode passed in as a parameter and the url of the database
     */
-    public boolean doesIdExist(ArrayList<String> collection, int id) {
-        for (String book : collection) {
+      public boolean doesBarcodeExist(int barcode, String url) {
+          Connection connection = null;
+          PreparedStatement preparedStatement = null;
+          ResultSet resultSet = null;
+
+          try {
+              // Establish a connection to the database using the provided databaseURL
+              connection = DriverManager.getConnection(url);
+
+              String selectSQL = "SELECT barcode FROM books WHERE barcode = ?";
+
+              // Prepare the SQL statement
+              preparedStatement = connection.prepareStatement(selectSQL);
+              preparedStatement.setInt(1, barcode);
+
+              // Execute the SELECT statement
+              resultSet = preparedStatement.executeQuery();
+
+              // If the result set contains a row, the barcode exists; otherwise, it doesn't
+              return resultSet.next();
+          } catch (SQLException e) {
+              e.printStackTrace();
+              return false; // An error occurred, treat it as if the barcode exists
+          } //end try catch
+          finally {
+                try {
+                  // Close the resources
+                  if (resultSet != null) {
+                      resultSet.close();
+                  }
+                  if (preparedStatement != null) {
+                      preparedStatement.close();
+                  }
+                  if (connection != null) {
+                      connection.close();
+                  }
+              } catch (SQLException e) {
+                  e.printStackTrace();
+              }//end try catch
+          }//end finally
+    }//end doesBarcodeExist
+
+
+    /*is called in the addBook method to add the newly created book to the database
+    the new book is passed in as a parameter returns false if the book cannot be added
+    */
+    private boolean insertBookToDatabase(String book) {
+        String url = "jdbc:sqlite:/Users/chrisfaris/collection.db";
+
+        try {
+            Connection connection = DriverManager.getConnection(url);
+
+            String sql = "INSERT INTO books (barcode, title, author, genre, status, due_date) VALUES (?, ?, ?, ?, ?, ?)";
+
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            // Parse the book details and set them in the prepared statement
             String[] bookDetails = book.split(",");
-            int existingId = Integer.parseInt(bookDetails[0]);
-            if (existingId == id) {
-                return true;
+            preparedStatement.setInt(1, Integer.parseInt(bookDetails[0])); // Barcode
+            preparedStatement.setString(2, bookDetails[1]); // Title
+            preparedStatement.setString(3, bookDetails[2]); // Author
+            preparedStatement.setString(4, bookDetails[3]); // Genre
+            preparedStatement.setString(5, bookDetails[4]); // Status
+            preparedStatement.setString(6, bookDetails[5]); // Due Date
+
+            // Execute the INSERT statement
+            int rowsAffected = preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+
+            // Check if the insertion was successful
+            return rowsAffected > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            // Handle the SQL exception and provide a meaningful error message
+            JOptionPane.showMessageDialog(null, "Failed to add the book to the collection. Please check your input.");
+            return false;
+        }//end try catch
+    }//end insertBookToDatabase
+
+
+    /*connects to the database and allows the user to enter a barcode of a book they wish to check back in
+    where the due date is then updated to null and the status is updated to checked in
+    has no parameters and no return
+    */
+    public void checkInBook() {
+        String url = "jdbc:sqlite:/Users/chrisfaris/collection.db";
+        int barcode = 0;
+
+        while (true) {
+            String barcodeInput = JOptionPane.showInputDialog("Please enter a barcode:");
+            if (barcodeInput == null) {
+                // User clicked Cancel or closed the input dialog
+                return;
             }//end if
-        }//end for
-        return false;
-    }//end doesIdExist
+
+            barcode = Integer.parseInt(barcodeInput);
+
+            // Check if the barcode exists in the database
+            if (!doesBarcodeExist(barcode, url)) {
+                JOptionPane.showMessageDialog(null, "Barcode does not exist in the database. Please enter a valid barcode.");
+            } else {
+                break; // Barcode exists, exit the loop
+            }//end if else
+        }//end while
+
+        try {
+            Connection connection = DriverManager.getConnection(url);
+            String sql = "UPDATE books SET status = 'checked in', due_date = NULL WHERE barcode = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            // Set the barcode value as a parameter
+            preparedStatement.setInt(1, barcode);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }//end try catch
+        JOptionPane.showMessageDialog(null, "Book successfully checked in.");
+
+    }//end checkInBook
+
+
+    /*connects to the database and allows the user to enter a barcode of a book they wish to check out
+    where the due date is then updated to 4 weeks from the current date and the status is updated to checked out
+    has no parameters and no return
+     */
+    public void checkOutBook() {
+        String url = "jdbc:sqlite:/Users/chrisfaris/collection.db";
+        int barcode = 0;
+
+        while (true) {
+            String barcodeInput = JOptionPane.showInputDialog("Please enter a barcode:");
+            if (barcodeInput == null) {
+                // User clicked Cancel or closed the input dialog
+                return;
+            }//end if
+
+            barcode = Integer.parseInt(barcodeInput);
+
+            // Check if the barcode exists in the database
+            if (!doesBarcodeExist(barcode, url)) {
+                JOptionPane.showMessageDialog(null, "Barcode does not exist in the database. Please enter a valid barcode.");
+            } else {
+                break; // Barcode exists, exit the loop
+            }//end if else
+        }//end while
+
+        try {
+            Connection connection = DriverManager.getConnection(url);
+            String sql = "UPDATE books SET status = 'checked out', due_date = date('now', '+28 days') WHERE barcode = ?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+
+            // Set the barcode value as a parameter
+            preparedStatement.setInt(1, barcode);
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }//end try catch
+        JOptionPane.showMessageDialog(null, "Book successfully checked out.");
+
+    }//end checkOutBook
 
     //creates a new instance of the Main class using SwingUtilities to ensure the GUI functions properly
     public static void main(String[] args) {
